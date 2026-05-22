bd5de011d01db1569e1a548156c6fa06fbeaee8a
Pavan Kumar H N <pavankumarn6997@gmail.com>
2026-05-22 11:56:46 +0530

    cilium helm
    Signed-off-by: Pavan Kumar H N <pavankumarn6997@gmail.com>

---
+++ cilium-helm/cilium.tf
@@ -0,0 +1,7 @@
+resource "helm_release" "cilium" {
+  name       = "cilium"
+  repository = "https://helm.cilium.io/"
+  chart      = "cilium"
+  version    = "1.19.4"
+  values     = [file("cilium-values.yaml")]
+}
